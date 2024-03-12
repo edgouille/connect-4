beca12658e6d26ef9b635d63931b523706df0840
Karl. D <karldamb27@gmail.com>
2024-03-12 12:14:50 +0100

    added play method

--- team.py
+++ team.py
@@ -2,8 +2,16 @@ class Team:
     def __init__(self, number, name):
         self.number = number
         self.name = name
+        
+    def get_number(self):
+        return self.number
     
     def play(self):
-        while choice < 1 and choice > 7:
-            choice = int(input("Choose a column to play in"))
-        return choice 
+        choice = int(input("Choose a column to play in"))
+        while choice < 1 or choice > 7:
+            user_input = input("Choose a column to play in : \n")
+            choice = int(user_input)
+            if choice < 1 or choice > 7:
+                print("Please choose a number between 1 and 7")
+        return choice
+    
