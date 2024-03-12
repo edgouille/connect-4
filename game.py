ae1884282e1692ddb8f6cb74838cf92eff07b87d
Karl. D <karldamb27@gmail.com>
2024-03-12 12:26:14 +0100

    added turn method

--- game.py
+++ game.py
@@ -2,18 +2,30 @@ from team import Team
 
 class Game:
     def __init__(self):
-        self.team1 = Team(1)
-        self.team2 = Team(2)
+        self.team1 = Team(1, "team1")
+        self.team2 = Team(2, "team2")
         self.board = Board()
         self.turn = 1
     
     def play(self):
         while True:
-            if self.Turn % 2 == 1:
-                self.Board.check_plays(self.Team1.play())
+            if self.turn % 2 == 1:
+                self.turn(self.team1)    
             else:
-                self.Team2.play()
-            self.Turn += 1
+                self.turn(self.team2)  
+            if self.board.check_win() != 0:
+                print("The winner is team ", self.board.check_win())
+                break
+            self.turn += 1
             
+            
+    def turn(self, team):
+        while True:
+            team_choice = team.play()
+            if self.board.check_plays(team_choice):
+                self.board.place_piece(team.get_number(), team_choice)
+                break
+            else:
+                print("This column is full, please choose another one.")
             
     
