e29e662adcfb89bd004432e028219fb3c76f557c
Karl. D <karldamb27@gmail.com>
2024-03-15 09:48:04 +0100

    added check_win method + added -=1 to column in place_piece

--- board.py
+++ board.py
@@ -12,6 +12,7 @@ class Board:
         return False
 
     def place_piece(self, team, num_column):
+        num_column -= 1
         line = 0
         column = self.board[:, num_column]
         if self.check_play(num_column):
@@ -21,4 +22,59 @@ class Board:
             self.board[line][num_column] = team
             print(f"l'equipe {team} à mis une piece a la position {line} {num_column}")
         else:
-            print("vous en pouvez pas choisir cette colonne")
+            print("vous ne pouvez pas choisir cette colonne")
+
+    def check_win(self, board):
+        if self.checkline(board) != 0:
+            return self.checkline(board)
+        if self.check_column(board) != 0:
+            return self.check_column(board)
+        if self.check_diagonal(board) != 0:
+            return self.check_diagonal(board)
+        if self.check_diagonal2(board) != 0:
+            return self.check_diagonal2(board)
+        return 0
+        
+    def checkline(board):
+        for i in range(6):
+            for j in range(4):
+                if board[i][j] == 1:
+                    if board[i][j+1] == 1 and board[i][j+2] == 1 and board[i][j+3] == 1:
+                        return 1
+                if board[i][j] == 2:
+                    if board[i][j+1] == 2 and board[i][j+2] == 2 and board[i][j+3] == 2:
+                        return 2
+        return 0   
+
+    def check_column(board):
+        for i in range(3):
+            for j in range(7):
+                if board[i][j] == 1:
+                    if board[i+1][j] == 1 and board[i+2][j] == 1 and board[i+3][j] == 1:
+                        return 1
+                if board[i][j] == 2:
+                    if board[i+1][j] == 2 and board[i+2][j] == 2 and board[i+3][j] == 2:
+                        return 2
+        return 0
+
+    def check_diagonal(board):
+        for i in range(3):
+            for j in range(4):
+                if board[i][j] == 1:
+                    if board[i+1][j+1] == 1 and board[i+2][j+2] == 1 and board[i+3][j+3] == 1:
+                        return 1
+                if board[i][j] == 2:
+                    if board[i+1][j+1] == 2 and board[i+2][j+2] == 2 and board[i+3][j+3] == 2:
+                        return 2
+        return 0
+
+    def check_diagonal2(board):
+        for i in range(3):
+            for j in range(7):
+                if board[i][j] == 1:
+                    if board[i+1][j-1] == 1 and board[i+2][j-2] == 1 and board[i+3][j-3] == 1:
+                        return 1
+                if board[i][j] == 2:
+                    if board[i+1][j-1] == 2 and board[i+2][j-2] == 2 and board[i+3][j-3] == 2:
+                        return 2
+        return 0
